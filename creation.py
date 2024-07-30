c7d475e79b43985aa2b5bf0fccf6530acc9941da
Seth <jamesbond1244@yahoo.co.uk>
2024-07-30 01:52:42 +0100

    new project

---
+++ creation.py
@@ -0,0 +1,13 @@
+import turtle
+
+
+
+turtle.bgcolor('blue')
+
+squary = turtle.Turtle()
+squary.speed(20)
+squary.pencolor("yellow")
+
+for i in range (400):
+   squary.forward(i)
+   squary.left(91)
